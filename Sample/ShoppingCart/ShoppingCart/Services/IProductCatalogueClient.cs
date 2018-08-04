ae3d797b03b53f11ddb34bed6b7ec1f0222a2039
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-05 00:12:55 +0700

    add RequestProductFromProductCatalogue

---
+++ Sample/ShoppingCart/ShoppingCart/Services/IProductCatalogueClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ShoppingCart.ShoppingCart;
+
+namespace ShoppingCart.Services
+{
+    public interface IProductCatalogueClient
+    {
+        Task<IEnumerable<ShoppingCartItem>> GetShoppingCartItems(int[] productCatalogueIds);
+    }
+}
---
+++ Sample/ShoppingCart/ShoppingCart/Services/ProductCatalogueClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Services
+{
+    public class ProductCatalogueClient
+    {
+        private static string productCatalogBaseUrl = @"http://private-05cc8-chapter2productcatalogmicroservice.apiary-mock.com";
+        private static string getProductPathTemplate = "/products?productIds=[{0}]";
+
+        private static async Task<HttpResponseMessage> RequestProductFromProductCatalogue(int[] productCatalogueIds)
+        {
+            var productsResource = string.Format(getProductPathTemplate, string.Join(",", productCatalogueIds));
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(productCatalogBaseUrl);
+                return await httpClient.GetAsync(productsResource).ConfigureAwait(false);
+            }
+        }
+    }
+}
