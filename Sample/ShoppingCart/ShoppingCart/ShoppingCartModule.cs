79692b0e154ec9b99bf5d28dd0192ca16f2e21f7
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-04 22:57:34 +0700

    Add shopping cart demo microservice

---
+++ Sample/ShoppingCart/ShoppingCart/ShoppingCartModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace ShoppingCart
+{
+    public class ShoppingCartModule: NancyModule
+    {
+        public ShoppingCartModule(
+            IShoppingCartStore shoppingCartStore)
+            : base("/shoppingcart")
+        {
+            Get("/{userid:int}", parameters =>
+            {
+                var userId = (int)parameters.userid;
+                return shoppingCartStore.Get(userId);
+            });
+
+        }
+    }
+
+    public interface IShoppingCartStore
+    {
+        ShoppingCart Get(int userId);
+        void Save(ShoppingCart shoppingCart);
+    }
+
+    public class ShoppingCart
+    {
+        public int UserId { get; }
+
+        public ShoppingCart(int userId)
+        {
+            this.UserId = userId;
+        }
+    }
+}
