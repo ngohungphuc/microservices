f2657761e26f7988171b3ec1cfcac75d98a2aebd
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-05 14:43:00 +0700

    add event feed module

---
+++ Sample/Chap2/ShoppingCart/ShoppingCart/EventFeed/EventsFeedModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace ShoppingCart.EventFeed
+{
+    public class EventsFeedModule : NancyModule
+    {
+        public EventsFeedModule(IEventStore eventStore) : base("/events")
+        {
+            Get("/", _ =>
+            {
+                long firstEventSequenceNumber, lastEventSequenceNumber;
+                if (!long.TryParse(this.Request.Query.start.Value,
+                    out firstEventSequenceNumber))
+                    firstEventSequenceNumber = 0;
+
+                if (!long.TryParse(this.Request.Query.end.Value,
+                    out lastEventSequenceNumber))
+                    lastEventSequenceNumber = long.MaxValue;
+
+                //Returns the raw list of events.
+                //Nancy takes care of serializing the
+                //events into the response body.
+                return
+                    eventStore.GetEvents(
+                        firstEventSequenceNumber,
+                        lastEventSequenceNumber);
+            });
+        }
+    }
+}
