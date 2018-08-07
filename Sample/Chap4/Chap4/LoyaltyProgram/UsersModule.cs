3266a78ecbe0d31a9ab874f1f2e94d71bcff1f8b
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-07 15:20:22 +0700

    add LoyaltyProgramUser class

---
+++ Sample/Chap4/Chap4/LoyaltyProgram/UsersModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoyaltyProgram
+{
+    public class UsersModule
+    {
+    }
+
+    public class LoyaltyProgramUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int LoyaltyPoints { get; set; }
+        public LoyaltyProgramSettings Settings { get; set; }
+    }
+
+    public class LoyaltyProgramSettings
+    {
+        public string[] Interests { get; set; }
+    }
+}
