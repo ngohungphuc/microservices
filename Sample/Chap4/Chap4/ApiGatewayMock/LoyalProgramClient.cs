9331c6c33ef4b75ecdcb3719cb91ddf3c995b852
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-08 23:48:48 +0700

    The API Gateway microservice registering new users

---
+++ Sample/Chap4/Chap4/ApiGatewayMock/LoyalProgramClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ApiGatewayMock
+{
+    public class LoyalProgramClient
+    {
+        private string hostName;
+
+        public async Task<LoyaltyProgramUser> RegisterUser(LoyaltyProgramUser newUser)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri($"http://{this.hostName}");
+                var response = await httpClient.PostAsync("/users/",
+                    new StringContent(JsonConvert.SerializeObject(newUser), Encoding.UTF8, "application/json"));
+
+                ThrowOnTransientFailure(response);
+                return JsonConvert.DeserializeObject<LoyaltyProgramUser>(await response.Content.ReadAsStringAsync());
+            }
+        }
+        private static void ThrowOnTransientFailure(HttpResponseMessage response)
+        {
+            if (((int)response.StatusCode) < 200 || ((int)response.StatusCode) > 499) throw new Exception(response.StatusCode.ToString());
+        }
+    }
+}
