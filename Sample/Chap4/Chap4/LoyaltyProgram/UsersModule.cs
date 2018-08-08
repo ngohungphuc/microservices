0d1bb63f86f8e8e348db8bcc5f9139df808ebe77
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-08 22:43:50 +0700

    POST endpoint for registering users

--- Sample/Chap4/Chap4/LoyaltyProgram/UsersModule.cs
+++ Sample/Chap4/Chap4/LoyaltyProgram/UsersModule.cs
@@ -2,23 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Nancy;
+using Nancy.ModelBinding;
 
 namespace LoyaltyProgram
 {
-    public class UsersModule
+    public sealed class UsersModule : NancyModule
     {
-    }
+        public UsersModule() : base("/users")
+        {
+            Post("/", _ =>
+            {
+                var newUser = this.Bind<LoyaltyProgramUser>();
+                this.AddRegisteredUser(newUser);
+                return this.CreatedResponse(newUser);
+            });
+        }
+        private dynamic CreatedResponse(LoyaltyProgramUser newUser)
+        {
+            return Negotiate.WithStatusCode(HttpStatusCode.Created)
+                .WithHeader("Location", $"{Request.Url.SiteBase}/users/{newUser.Id}").WithModel(newUser);
+        }
 
-    public class LoyaltyProgramUser
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int LoyaltyPoints { get; set; }
-        public LoyaltyProgramSettings Settings { get; set; }
-    }
-
-    public class LoyaltyProgramSettings
-    {
-        public string[] Interests { get; set; }
+        private void AddRegisteredUser(LoyaltyProgramUser newUser)
+        {
+            // store the newUser to a data store
+        }
     }
 }
+
+public class LoyaltyProgramUser
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int LoyaltyPoints { get; set; }
+    public LoyaltyProgramSettings Settings { get; set; }
+}
+
+public class LoyaltyProgramSettings
+{
+    public string[] Interests { get; set; }
+}
+
