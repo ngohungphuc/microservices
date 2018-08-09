1d5ef5cc068e1f2b970821dba92c586647f7340a
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-09 22:06:01 +0700

    Serializing to YAML

--- Sample/Chap4/Chap4/LoyaltyProgram/YamlSerializerDeserializer.cs
+++ Sample/Chap4/Chap4/LoyaltyProgram/YamlSerializerDeserializer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Nancy;
 using Nancy.ModelBinding;
 using Nancy.Responses.Negotiation;
 using YamlDotNet.Serialization;
@@ -32,6 +33,62 @@ namespace LoyaltyProgram
             var yamlDeserializer = new Deserializer();
             var reader = new StreamReader(bodyStream);
             return yamlDeserializer.Deserialize(reader, context.DestinationType);
+        }
+    }
+
+    public class YamlBodySerializer : IResponseProcessor
+    {
+        /// <summary>
+        /// Tells Nancy that this processor can handle accept header values that end with “yaml”
+        /// </summary>
+        /// <param name="requestedMediaRange"></param>
+        /// <param name="model"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public ProcessorMatch CanProcess(MediaRange requestedMediaRange, dynamic model, NancyContext context)
+        {
+            return requestedMediaRange.Subtype.ToString().EndsWith("yaml")
+                ? new ProcessorMatch
+                {
+                    ModelResult = MatchResult.DontCare,
+                    RequestedContentTypeResult = MatchResult.NonExactMatch
+                }
+                : ProcessorMatch.None;
+        }
+
+        /// <summary>
+        /// Creates a new response object to use in the rest of Nancy’s pipeline
+        /// </summary>
+        /// <param name="requestedMediaRange"></param>
+        /// <param name="model"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public Response Process(MediaRange requestedMediaRange, dynamic model, NancyContext context)
+        {
+            return new Response
+            {
+                //Sets up a function that writes the response body to a stream
+                Contents = stream =>
+                {
+                    var yamlSerializer = new Serializer();
+                    var streamWriter = new StreamWriter(stream);
+                    //Writes the YAML serialized object to the stream Nancy uses for  the response body
+                    yamlSerializer.Serialize(streamWriter, model);
+                    streamWriter.Flush();
+                },
+                ContentType = "application/yaml"
+            };
+        }
+
+        /// <summary>
+        /// Tells Nancy which file extensions can be handled by this response processor
+        /// </summary>
+        public IEnumerable<Tuple<string, MediaRange>> ExtensionMappings
+        {
+            get
+            {
+                yield return new Tuple<string, MediaRange>("yaml", new MediaRange("application/yaml"));
+            }
         }
     }
 }
