3c8ee0ca8091d463392cb8e7e3ea0d812dbe0907
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-09 23:26:10 +0700

    add LoyaltyProgramEventConsumer project

---
+++ Sample/Chap4/Chap4/LoyaltyProgramEventConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoyaltyProgramEventConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
