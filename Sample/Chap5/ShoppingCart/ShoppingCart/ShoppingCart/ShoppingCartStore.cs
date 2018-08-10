ec40385e7938ab55834c2050a6adcc6e6e2ef839
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-10 23:58:59 +0700

    Writing shopping carts to the database

--- Sample/Chap5/ShoppingCart/ShoppingCart/ShoppingCart/ShoppingCartStore.cs
+++ Sample/Chap5/ShoppingCart/ShoppingCart/ShoppingCart/ShoppingCartStore.cs
@@ -14,22 +14,40 @@ namespace ShoppingCart.ShoppingCart
             Integrated Security=True";
 
         private const string readItemsSql =
-                @"select * from ShoppingCart, ShoppingCartItems
-                where ShoppingCartItems.ShoppingCartId = ID
-                and ShoppingCart.UserId=@UserId";
+            @"select * from ShoppingCart, ShoppingCartItems
+            where ShoppingCartItems.ShoppingCartId = ID
+            and ShoppingCart.UserId=@UserId";
+
+        private const string deleteAllForShoppingCartSql =
+            @"delete item from ShoppingCartItems item
+            inner join ShoppingCart cart on item.ShoppingCartId = cart.ID
+            and cart.UserId=@UserId";
+
+        private const string addAllForShoppingCartSql =
+             @"insert into ShoppingCartItems
+            (ShoppingCartId, ProductCatalogId, ProductName,
+            ProductDescription, Amount, Currency)
+            values
+            (@ShoppingCartId, @ProductCatalogId, @ProductName,v
+            @ProductDescription, @Amount, @Currency)";
 
         public async Task<ShoppingCart> Get(int userId)
         {
             using (var conn = new SqlConnection(connectionString))
             {
-                var items = await conn.QueryAsync<ShoppingCartItem>(readItemsSql, new {UserId = userId});
+                var items = await conn.QueryAsync<ShoppingCartItem>(readItemsSql, new { UserId = userId });
                 return new ShoppingCart(userId, items);
             }
         }
 
-        public Task Save(ShoppingCart shoppingCart)
+        public async Task Save(ShoppingCart shoppingCart)
         {
-            throw new NotImplementedException();
+            using (var conn = new SqlConnection(connectionString))
+            using (var tx = conn.BeginTransaction())
+            {
+                await conn.ExecuteAsync(deleteAllForShoppingCartSql, new { shoppingCart.UserId }).ConfigureAwait(false);
+                await conn.ExecuteAsync(addAllForShoppingCartSql, shoppingCart.Items, tx).ConfigureAwait(false);
+            }
         }
     }
 }
