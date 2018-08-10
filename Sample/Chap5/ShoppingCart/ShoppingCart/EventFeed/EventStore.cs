eb22d7ddcac9bb1c33fdf736616f339029cd4409
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-11 00:32:57 +0700

    EventStore method for reading events

--- Sample/Chap5/ShoppingCart/ShoppingCart/EventFeed/EventStore.cs
+++ Sample/Chap5/ShoppingCart/ShoppingCart/EventFeed/EventStore.cs
@@ -18,17 +18,32 @@ namespace ShoppingCart.EventFeed
             @"insert into EventStore(Name, OccurredAt, Content) values
               (@Name, @OccurredAt, @Content)";
 
+        private const string readEventsSql =
+            @"select * from EventStore where ID >= @Start and ID <= @End";
+
         private static long currentSequenceNumber = 0;
         private static readonly IList<Event> database = new List<Event>();
 
         public async Task<IEnumerable<Event>> GetEvents(
             long firstEventSequenceNumber,
             long lastEventSequenceNumber)
-            => database
-                .Where(e =>
-                    e.SequenceNumber >= firstEventSequenceNumber &&
-                    e.SequenceNumber <= lastEventSequenceNumber)
-                .OrderBy(e => e.SequenceNumber);
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                return (await conn.QueryAsync<dynamic>(
+                        readEventsSql,
+                        new
+                        {
+                            Start = firstEventSequenceNumber,
+                            End = lastEventSequenceNumber
+                        }).ConfigureAwait(false))
+                    .Select(row =>
+                    {
+                        var content = JsonConvert.DeserializeObject(row.Content);
+                        return new Event(row.Id, row.OccurredAt, row.Name, content);
+                    });
+            }
+        }
 
         public Task Raise(string eventName, object content)
         {
