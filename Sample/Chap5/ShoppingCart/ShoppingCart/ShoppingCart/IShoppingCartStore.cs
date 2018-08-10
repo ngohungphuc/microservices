e7d47b24f3b15b9231c5e30995a81196d5a8c69d
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-10 23:35:53 +0700

    setup

---
+++ Sample/Chap5/ShoppingCart/ShoppingCart/ShoppingCart/IShoppingCartStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.ShoppingCart
+{
+    public interface IShoppingCartStore
+    {
+        Task<ShoppingCart> Get(int userId);
+        Task Save(ShoppingCart shoppingCart);
+    }
+}
