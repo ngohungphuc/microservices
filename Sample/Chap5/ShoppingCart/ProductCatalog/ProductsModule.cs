0f4aedebea8e93ddaadc5e67ac957d9372af0edb
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-11 12:55:54 +0700

    Adding cache headers to the product list

---
+++ Sample/Chap5/ShoppingCart/ProductCatalog/ProductsModule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nancy;
+
+namespace ProductCatalog
+{
+    public class ProductsModule : NancyModule
+    {
+        public ProductsModule(ProductStore productStore) : base("/products")
+        {
+            Get("", _ =>
+            {
+                string productIdsString = this.Request.Query.productIds;
+                var productIds = ParseProductIdsFromQueryString(productIdsString);
+                var products = productStore.GetProductsByIds(productIds);
+
+                return this.Negotiate.WithModel(products).WithHeader("cache-control", "max-age:86400");
+            });
+        }
+
+        private IEnumerable<int>
+            ParseProductIdsFromQueryString(string productIdsString)
+        {
+            
+        }
+    }
+    public interface ProductStore
+    {
+        IEnumerable<ProductCatalogProduct> GetProductsByIds(IEnumerable<int> productIds);
+    }
+
+    public class ProductCatalogProduct
+    {
+        public ProductCatalogProduct(int productId, string productName, string description, Money price)
+        {
+            this.ProductId = productId.ToString();
+            this.ProductName = productName;
+            this.ProductDescription = description;
+            this.Price = price;
+        }
+        public string ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public string ProductDescription { get; private set; }
+        public Money Price { get; private set; }
+    }
+
+    public class Money { }
+}
