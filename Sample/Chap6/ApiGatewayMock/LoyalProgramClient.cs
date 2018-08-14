f8d3e71b965f9408d77f6451de5c3f33cc610e52
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-14 14:37:46 +0700

    Using a Polly policy around an HTTP request

--- Sample/Chap6/ApiGatewayMock/LoyalProgramClient.cs
+++ Sample/Chap6/ApiGatewayMock/LoyalProgramClient.cs
@@ -26,16 +26,20 @@ namespace ApiGatewayMock
             }
         }
 
-        public async Task<LoyaltyProgramUser> RegisterUser(LoyaltyProgramUser newUser)
+        public async Task<HttpResponseMessage> RegisterUser(LoyaltyProgramUser newUser)
+        {
+            return await exponentialRetryPolicy.ExecuteAsync(() => DoRegisterUser(newUser));
+        }
+
+        private async Task<HttpResponseMessage> DoRegisterUser(LoyaltyProgramUser newUser)
         {
             using (var httpClient = new HttpClient())
             {
                 httpClient.BaseAddress = new Uri($"http://{this.hostName}");
-                var response = await httpClient.PostAsync("/users/",
-                    new StringContent(JsonConvert.SerializeObject(newUser), Encoding.UTF8, "application/json"));
+                var response = await httpClient.PostAsync("/users/", new StringContent(JsonConvert.SerializeObject(newUser), Encoding.UTF8, "application/json"));
 
                 ThrowOnTransientFailure(response);
-                return JsonConvert.DeserializeObject<LoyaltyProgramUser>(await response.Content.ReadAsStringAsync());
+                return response;
             }
         }
 
@@ -57,4 +61,54 @@ namespace ApiGatewayMock
             if (((int)response.StatusCode) < 200 || ((int)response.StatusCode) > 499) throw new Exception(response.StatusCode.ToString());
         }
     }
+
+    //public class LoyalProgramClient
+    //{
+    //    private string hostName;
+    //    private static Policy exponentialRetryPolicy = Policy.Handle<Exception>().WaitAndRetryAsync(3, attempt => TimeSpan.FromMilliseconds(100 * Math.Pow(2, attempt)));
+
+    //    public async Task<LoyaltyProgramUser> QueryUser(int userId)
+    //    {
+    //        var userResource = $"/users/{userId}";
+    //        using (var httpClient = new HttpClient())
+    //        {
+    //            httpClient.BaseAddress = new Uri($"http://{this.hostName}");
+    //            var response = await httpClient.GetAsync(userResource);
+
+    //            ThrowOnTransientFailure(response);
+    //            return JsonConvert.DeserializeObject<LoyaltyProgramUser>(await response.Content.ReadAsStringAsync());
+    //        }
+    //    }
+
+    //    public async Task<LoyaltyProgramUser> RegisterUser(LoyaltyProgramUser newUser)
+    //    {
+    //        using (var httpClient = new HttpClient())
+    //        {
+    //            httpClient.BaseAddress = new Uri($"http://{this.hostName}");
+    //            var response = await httpClient.PostAsync("/users/",
+    //                new StringContent(JsonConvert.SerializeObject(newUser), Encoding.UTF8, "application/json"));
+
+    //            ThrowOnTransientFailure(response);
+    //            return JsonConvert.DeserializeObject<LoyaltyProgramUser>(await response.Content.ReadAsStringAsync());
+    //        }
+    //    }
+
+    //    public async Task<LoyaltyProgramUser> UpdateUser(LoyaltyProgramUser user)
+    //    {
+    //        using (var httpClient = new HttpClient())
+    //        {
+    //            httpClient.BaseAddress = new Uri($"http://{this.hostName}");
+    //            var response = await httpClient.PutAsync($"/users/{user.Id}",
+    //                new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json"));
+
+    //            ThrowOnTransientFailure(response);
+    //            return JsonConvert.DeserializeObject<LoyaltyProgramUser>(await response.Content.ReadAsStringAsync());
+    //        }
+    //    }
+
+    //    private static void ThrowOnTransientFailure(HttpResponseMessage response)
+    //    {
+    //        if (((int)response.StatusCode) < 200 || ((int)response.StatusCode) > 499) throw new Exception(response.StatusCode.ToString());
+    //    }
+    //}
 }
